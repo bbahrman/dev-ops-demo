28d570159a014ebf479de866e77fc05be7bb7c65
Ben Bahrman <ben.bahrman@headintheclouddev.com>
2024-12-14 20:33:36 -0600

    [ticket-200] setting original quantity

--- TypeScripts/SuiteScripts/demo.ts
+++ TypeScripts/SuiteScripts/demo.ts
@@ -4,29 +4,38 @@
  * @NScriptType UserEventScript
  */
 
-import type {EntryPoints} from 'N/types';
-import query = require('N/query');
-import runtime = require('N/runtime');
-import log = require('N/log');
+import type { EntryPoints } from "N/types";
+import query = require("N/query");
+import runtime = require("N/runtime");
+import log = require("N/log");
 
 export const beforeLoad: EntryPoints.UserEvent.beforeLoad = (beforeLoadContext) => {
-  const soLookup: ISQLSalesOrder[] = query.runSuiteQL({
-    query: 'SELECT id, type, entity, custbody_demo as some_field FROM transaction'
-  }).asMappedResults() as any;
+  const soLookup: ISQLSalesOrder[] = query
+    .runSuiteQL({
+      query: "SELECT id, type, entity, custbody_demo as some_field FROM transaction",
+    })
+    .asMappedResults() as any;
 
   const firstSO = soLookup[0].id;
-  log.debug({ title: 'beforeLoad', details: firstSO });
-}
+  log.debug({ title: "beforeLoad", details: firstSO });
+};
 
-export const beforeSubmit: EntryPoints.UserEvent.beforeSubmit = (context) => {
+export const beforeSubmit: EntryPoints.UserEvent.beforeSubmit = (beforeSubmitContext) => {
+  const rec = beforeSubmitContext.newRecord;
   if (runtime.ContextType.WEBSERVICES === runtime.executionContext) {
-    context.newRecord.setValue({ fieldId: 'custbody_integrated', value: true });
+    rec.setValue({ fieldId: "custbody_integrated", value: true });
   }
-}
+  const sublistId = "item";
+  const lineCount = rec.getLineCount({ sublistId });
+  for (let line = 0; line < lineCount; line++) {
+    const value = rec.getSublistValue({ sublistId, fieldId: "quantity", line });
+    rec.setSublistValue({ sublistId, fieldId: "custcol_original_quantity", line, value });
+  }
+};
 
 interface ISQLSalesOrder {
   id: number;
   type: string;
   entity: number;
-  some_field: "T"|"F";
+  some_field: "T" | "F";
 }
--- src/FileCabinet/SuiteScripts/demo.js
+++ src/FileCabinet/SuiteScripts/demo.js
@@ -3,16 +3,31 @@
  * @NModuleScope Public
  * @NScriptType UserEventScript
  */
-define(["require", "exports", "N/query", "N/log"], function (require, exports, query, log) {
+define(["require", "exports", "N/query", "N/runtime", "N/log"], function (require, exports, query, runtime, log) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.beforeLoad = void 0;
+    exports.beforeSubmit = exports.beforeLoad = void 0;
     const beforeLoad = (beforeLoadContext) => {
-        const soLookup = query.runSuiteQL({
-            query: `SELECT id, type, entity, custbody_demo as some_field FROM transaction`
-        }).asMappedResults();
+        const soLookup = query
+            .runSuiteQL({
+            query: "SELECT id, type, entity, custbody_demo as some_field FROM transaction",
+        })
+            .asMappedResults();
         const firstSO = soLookup[0].id;
-        log.debug({ title: 'beforeLoad', details: firstSO });
+        log.debug({ title: "beforeLoad", details: firstSO });
     };
     exports.beforeLoad = beforeLoad;
+    const beforeSubmit = (beforeSubmitContext) => {
+        const rec = beforeSubmitContext.newRecord;
+        if (runtime.ContextType.WEBSERVICES === runtime.executionContext) {
+            rec.setValue({ fieldId: "custbody_integrated", value: true });
+        }
+        const sublistId = "item";
+        const lineCount = rec.getLineCount({ sublistId });
+        for (let line = 0; line < lineCount; line++) {
+            const value = rec.getSublistValue({ sublistId, fieldId: "quantity", line });
+            rec.setSublistValue({ sublistId, fieldId: "custcol_original_quantity", line, value });
+        }
+    };
+    exports.beforeSubmit = beforeSubmit;
 });
